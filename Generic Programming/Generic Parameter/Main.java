b5ff65e2baa295dd4569f6c9f5eda0e477569d6b
Aniket Gupta <aniketgupta3001@gmail.com>
2023-09-24 01:19:30 +0530

    Delete Generic Programming directory

--- Generic Programming/Generic Parameter/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String args[]) {
-        MyGenericList <Pair<String, Integer>> db = new MyGenericList <Pair<String, Integer>>();
-        db.add(new Pair <String, Integer> ("John", 2343));
-        db.add(new Pair <String, Integer> ("Susane", 8908));
-    }
-}
