cb6068124cce9256d476986d818da87c13cd4e6b
Aniket Gupta <aniketgupta3001@gmail.com>
2023-10-26 00:34:05 +0530

    Delete Assignments/A2 directory

--- Assignments/A2/Membership.java
+++
@@ -1,6 +0,0 @@
-package org.example;
-
-public interface Membership {
-    boolean isBasic();
-    boolean isPremium();
-}
--- Assignments/A2/SpecialDeal.java
+++
@@ -1,5 +0,0 @@
-package org.example;
-
-public interface SpecialDeal {
-    double applySpecialDeal(double ticketPrice, int totalTickets);
-}
